bb48e78e6f979f21dc7484d2d5c80b7719ff255b
Venkata Shiva Prasad <venkatashivaprasad.nannuri@gmail.com>
2025-01-30 14:52:58 +0530

    changes in the link tag

--- app/(marketing)/_components/_ui/Card.tsx
+++ app/(marketing)/_components/_ui/Card.tsx
@@ -52,11 +52,13 @@ const Card = (
                 />
               </CardItem>
               <div className="flex justify-between items-center mt-20">
+                
                 <CardItem
                   translateZ={20}
                   as={Link}
                   href={i.demo}
                   target="__blank"
+                  rel="noopener noreferrer"
                   className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
                 >
                     Try now →
